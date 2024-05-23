d9c48c2c2e693b65b1dcb5c8b8537a3a19a81fb4
Rounak Paul <rounakpaul@hotmail.com>
2024-05-23 08:59:14 +0530

    Update VulkanBackend.h

--- Quasar/src/Renderer/VulkanBackend/VulkanBackend.h
+++ Quasar/src/Renderer/VulkanBackend/VulkanBackend.h
@@ -27,8 +27,8 @@ namespace Quasar::RendererBackend
 
         const std::vector<Vertex> vertices = {
             {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-            {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+            {{0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}},
+            {{-0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}}
         };
 
         private:
